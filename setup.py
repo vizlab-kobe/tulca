88900311bee63b820d68b864cf64dd3eef099a69
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2025-08-06 15:57:27 +0900

    autograd to use manopt optimizer

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(
     version="0.1.0",
     packages=["tulca"],
     package_dir={"tulca": "tulca"},
-    install_requires=["numpy", "scipy", "tensorly", "factor-analyzer", "pymanopt"],
+    install_requires=[
+        "autograd",
+        "numpy",
+        "scipy",
+        "tensorly",
+        "factor-analyzer",
+        "pymanopt",
+    ],
     py_modules=["tulca"],
 )
